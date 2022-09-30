c4b69f734a74333f4ce38dbbd82d0cc957a5db7f
awssilva <wssalexandre@gmail.com>
2022-09-29 22:24:31 -0300

    feat: pdfGenerator implement

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module geradorpdf
 
 go 1.19
 
-require github.com/google/uuid v1.3.0
+require (
+    github.com/google/uuid v1.3.0
+    github.com/SebastiaanKlippert/go-wkhtmltopdf v1.7.2
+)
--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"pdf-go/htmlParser"
+	"pdf-go/pdfGenerator"
+	"os"
+)
+
+type Data struct {
+	Name string
+}
+
 func main() {
-	
+
+	h := htmlParser.New("tmp")
+	wk := pdfGenerator.NewWkHtmlToPDF("tmp")
+
+	dataHTML := Data{
+		Name: "Exemple Name Title",
+	}
+
+	htmlGenerated, err := h.Create("templates/example.html", dataHTML)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer os.Remove(htmlGenerated)
+	fmt.Println("HTML gerado", htmlGenerated)
+
+	filePDFName, err := wk.Create(htmlGenerated)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("PDF gerado", filePDFName)
+
 }
